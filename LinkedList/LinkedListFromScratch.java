3a46e8beb893e3b2d5a3ad8b67d13e18eea6e4b6
Chitwan Singh <110717037+Chitwan23@users.noreply.github.com>
2024-07-19 23:54:01 +0530

    LinkedListFromScratch.java

---
+++ LinkedList/LinkedListFromScratch.java
@@ -0,0 +1,85 @@
+package LL;
+public class Linkedlist {
+	Node Head;
+	int size;
+	 
+	public class Node {
+		int data;
+		Node next; 
+	
+	public Node(int data) {
+		this.data = data;
+	}
+	public Node(int data , Node next) {
+		this.data = data;
+		this.next = next;
+		
+	}}
+	
+	public void insert(int data) {
+		Node node = new Node( data);
+		if (Head ==null) {
+		Head = node;
+		size++;
+		return;}
+	 Node temp = Head;
+	 while(temp.next!=null) {
+		 temp = temp.next;
+	 }
+	 temp.next = node;
+	 size++;
+		
+	}
+	
+	public void display() {
+		if (Head== null) {
+			System.out.println("list is empty");
+		}
+	
+		Node temp = Head;
+	  while(temp!=null) {
+		System.out.print(temp.data + "-> ");
+		temp =temp.next;
+	}
+	  System.out.print("END");
+	}
+	
+	public void delete(int index) {
+		if (Head== null) {
+			System.out.print("list is empty");
+			return;
+		}
+		else if (index==0) {
+			Head=Head.next;	
+			return;
+		}
+		Node temp = Head;
+        for (int i = 0; i < index-1; i++) {
+            temp = temp.next;
+        }
+
+        temp.next = temp.next.next;
+        size--;
+    }
+	
+	
+	public static void main(String args[]) {
+		Linkedlist list = new Linkedlist();
+		list.insert(1);
+		list.insert(3);
+		list.insert(5);
+		list.insert(7);
+		list.display();
+		list.delete(0);
+		System.out.println();
+		list.display();
+		list.insert(2);
+		list.insert(8);
+		System.out.println();
+		list.display();
+		list.delete(3);
+		System.out.println();
+		list.display();
+	}
+
+}
