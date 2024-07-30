236e531949be2cdf1fcc73a8af3fb81ff4211126
Chitwan Singh <110717037+Chitwan23@users.noreply.github.com>
2024-07-30 15:30:16 +0530

    InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,20 +1,23 @@
-Class Main{
+import java.util.*;
+class Main{
   public static void InsertionSort (int[]arr){
     int n = arr.length;
-    for (int i =0; i<n-1; i++){
-      for (int j = i+1; j>0; j--){
-        if (arr[j]<arr[j-1]){
-          int temp = arr[j];
-          arr[j]= arr[j-1];
-          arr[j-1]= temp;
+    for (int i = 1; i<n; i++){
+      int j = i-1;
+      int current = arr[i];
+      while(j>=0 && arr[j]>current)
+      {arr[j+1]=arr[j];
+       j--;
         }
+      arr[j+1]= current ;
+
         
       }
     }
     public static void main(String args[]){
-      int [] arr = {3,6,8,2,7,1}
+      int [] arr = {3,6,8,2,7,1};
         InsertionSort(arr);
-      System.out.print(Arrays.toString(arr));
+      System.out.println(Arrays.toString(arr));
       
     }
   }
