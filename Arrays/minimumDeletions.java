eb0a75798a003b190acbd257589e242470b29990
Chitwan Singh <110717037+Chitwan23@users.noreply.github.com>
2024-07-31 23:17:18 +0530

    minimumDeletions.java

---
+++ Arrays/minimumDeletions.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int c = 0; int r = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i)=='b'){
+                c++;
+            }
+            else if (c!=0){
+                c--;
+                r++;
+            }
+        }
+        return r;
+    }
+}
